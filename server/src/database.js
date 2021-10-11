639f494a41769c24665b462d8e96917eda362049
EliHeuman <biz.elih@gmail.com>
2021-10-11 21:33:29 +0300

    Changed IP and ports to server's

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('./models/user.model');
-36df8ef818ba
+
 // const connection = 'mongodb://6c0fd72ffb74:27017/bankApp';
 const connection = 'mongodb://36df8ef818ba:27017/bankApp';
 const connectDb = () => {
